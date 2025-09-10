5501b5211d00eb92b9f4b18e9407df376afb6a69
nikitha84 <nikitha84640@gmail.com>
2025-09-10 13:47:56 +0530

    vpc-project2

--- data.tf
+++ data.tf
@@ -2,4 +2,14 @@ data "aws_availability_zones" "azs" {
   state = "available"
 
 }
-data ""
+data "aws_vpc" "default" { #for default vpc id
+  default = true
+}
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "association.main"
+    values = ["true"]
+  } 
+
+}
